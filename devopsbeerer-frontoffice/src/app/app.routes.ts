889df6e96a397b11b020f45a779aced372aa8681
Quentin RODIC <quentin.rodic@qiminfo.ch>
2025-05-14 08:02:40 +0200

    feat: add actions to beer to frontoffice

--- devopsbeerer-frontoffice/src/app/app.routes.ts
+++ devopsbeerer-frontoffice/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { autoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
 import { CallbackComponent } from './callback/callback.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import { FormComponent } from './pages/form/form.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent, canActivate: [autoLoginPartialRoutesGuard] },
+    { path: "form", component: FormComponent },
     { path: 'unauthorized', component: UnauthorizedComponent },
     { path: 'callback', component: CallbackComponent }
 ];
